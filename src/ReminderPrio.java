2778d86c2b22317750e59e6dca52bd511eb65900
RodneyOlid <117031324+RodneyOlid@users.noreply.github.com>
2022-11-29 23:47:41 -0500

    Delete ReminderPrio.java

--- src/ReminderPrio.java
+++
@@ -1,9 +0,0 @@
-public class ReminderPrio {
-    public static boolean reminderPriority = false;
-    public static void setPriority(String prio){
-        reminderPriority = prio.equals("yes");
-    }
-    public static boolean getPriority(){
-        return reminderPriority;
-    }
-}
